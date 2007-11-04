f499c0e2e2b7f87c789cf632a81646742c22460e
Ronald Landheer-Cieslak <ronald@landheer-cieslak.com>
2007-11-04 12:09:31 -0500

    Compile Spin as a shared library and fix the makefiles

--- lib/Spin/module.mk
+++ lib/Spin/module.mk
@@ -1,12 +1,17 @@
 # the library
 Spin_SRC =					\
 	Private/BakeryCounter.cpp		\
+	Private/ConnectionHandler.cpp		\
+	Private/OpenSSL.cpp			\
+	Private/Pipe.cpp			\
 	Private/Logger.cpp			\
+	Private/TLS.cpp				\
 	Private/atomicPrimitives.cpp		\
 	Private/yield.cpp			\
-	Private/TLS.cpp				\
-	Private/OpenSSL.cpp
-
+	Handlers/NewConnectionHandler.cpp	\
+	Connection.cpp				\
+	Connector.cpp				\
+	Listener.cpp
 
 Spin_INSTALL_HEADERS += $(filter-out Private/%, $(wildcard *.h) $(wildcard */*.h))
 
@@ -16,4 +21,4 @@ INSTALL_HEADERS += $(patsubst %,Spin/%,$(Spin_INSTALL_HEADERS))
 Spin_OBJ := $(patsubst %.cpp,lib/Spin/%.lo,$(Spin_SRC))
 OBJ += $(Spin_OBJ)
 
-$(eval $(call LINK_LIBRARY_template,libSpin.la,$(Spin_OBJ)))
+$(eval $(call LINK_LIBRARY_template,libSpin.la,$(Spin_OBJ),-whole-archive -version-info 0:0:0 -rpath $(libdir)))
--- tests/lib/Spin/module.mk
+++ tests/lib/Spin/module.mk
@@ -1,11 +1,20 @@
-Test_Spin_SRC :=			main.cpp BakeryCounter.cpp  Logger.cpp  atomicPrimitives.cpp   
+Test_Spin_SRC :=			\
+	BakeryCounter.cpp		\
+	Logger.cpp			\
+	atomicPrimitives.cpp		\
+	main.cpp			\
+	Pipe.cpp			\
+	Connection.cpp			\
+	Connector.cpp			\
+	Listener.cpp
+
 SRC += $(patsubst %,tests/lib/Spin/%,$(Test_Spin_SRC))
 
 Test_Spin_OBJ := $(patsubst %.cpp,tests/lib/Spin/%.lo,$(Test_Spin_SRC))
 
 OBJ += $(Test_Spin_OBJ)
 
-TEST_LDFLAGS=libSpin.la $(LIBS) -lcppunit -lltdl
+TEST_LDFLAGS=libSpin.la $(LIBS) -lcppunit -lltdl -lboost_filesystem-gcc-mt-1_33_1 -lssl -lcrypto -lboost_thread-gcc-mt-1_33_1
 $(eval $(call LINK_BINARY_template,Test_Spin.bin,$(Test_Spin_OBJ),$(TEST_LDFLAGS)))
 CHECK_DEPS += Test_Spin.bin 
 
