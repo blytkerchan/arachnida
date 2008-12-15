6bf48e7c510a1c467698baebdd25aa51df89d518
Michel Fortin <michel.fortin@optelvision.com>
2008-12-15 15:03:37 -0500

    Removed debug output from parseURL.

--- lib/Damon/Private/parseURL.cpp
+++ lib/Damon/Private/parseURL.cpp
@@ -26,18 +26,18 @@ namespace Damon
 				re_path = 10,
 			};
 
-			std::cerr << std::endl;
-			std::cerr << 0 << ": " << mr[0].matched << " " << std::string(mr[0].first, mr[0].second) << std::endl;
-			std::cerr << 1 << ": " << mr[1].matched << " " << std::string(mr[1].first, mr[1].second) << std::endl;
-			std::cerr << 2 << ": " << mr[2].matched << " " << std::string(mr[2].first, mr[2].second) << std::endl;
-			std::cerr << 3 << ": " << mr[3].matched << " " << std::string(mr[3].first, mr[3].second) << std::endl;
-			std::cerr << 4 << ": " << mr[4].matched << " " << std::string(mr[4].first, mr[4].second) << std::endl;
-			std::cerr << 5 << ": " << mr[5].matched << " " << std::string(mr[5].first, mr[5].second) << std::endl;
-			std::cerr << 6 << ": " << mr[6].matched << " " << std::string(mr[6].first, mr[6].second) << std::endl;
-			std::cerr << 7 << ": " << mr[7].matched << " " << std::string(mr[7].first, mr[7].second) << std::endl;
-			std::cerr << 8 << ": " << mr[8].matched << " " << std::string(mr[8].first, mr[8].second) << std::endl;
-			std::cerr << 9 << ": " << mr[9].matched << " " << std::string(mr[9].first, mr[9].second) << std::endl;
-			std::cerr << 10 << ": " << mr[10].matched << " \"" << std::string(mr[10].first, mr[10].second) << "\" " << std::string(mr[10].first, mr[10].second).size() << " " << std::string(mr[10].first, mr[10].second).empty() << std::endl;
+			//std::cerr << std::endl;
+			//std::cerr << 0 << ": " << mr[0].matched << " " << std::string(mr[0].first, mr[0].second) << std::endl;
+			//std::cerr << 1 << ": " << mr[1].matched << " " << std::string(mr[1].first, mr[1].second) << std::endl;
+			//std::cerr << 2 << ": " << mr[2].matched << " " << std::string(mr[2].first, mr[2].second) << std::endl;
+			//std::cerr << 3 << ": " << mr[3].matched << " " << std::string(mr[3].first, mr[3].second) << std::endl;
+			//std::cerr << 4 << ": " << mr[4].matched << " " << std::string(mr[4].first, mr[4].second) << std::endl;
+			//std::cerr << 5 << ": " << mr[5].matched << " " << std::string(mr[5].first, mr[5].second) << std::endl;
+			//std::cerr << 6 << ": " << mr[6].matched << " " << std::string(mr[6].first, mr[6].second) << std::endl;
+			//std::cerr << 7 << ": " << mr[7].matched << " " << std::string(mr[7].first, mr[7].second) << std::endl;
+			//std::cerr << 8 << ": " << mr[8].matched << " " << std::string(mr[8].first, mr[8].second) << std::endl;
+			//std::cerr << 9 << ": " << mr[9].matched << " " << std::string(mr[9].first, mr[9].second) << std::endl;
+			//std::cerr << 10 << ": " << mr[10].matched << " \"" << std::string(mr[10].first, mr[10].second) << "\" " << std::string(mr[10].first, mr[10].second).size() << " " << std::string(mr[10].first, mr[10].second).empty() << std::endl;
 
 			std::string protocol(mr[re_protocol].matched ? std::string(mr[re_protocol].first, mr[re_protocol].second) : "http");
 			if (protocol != "http" && protocol != "https")
